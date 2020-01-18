b1136b95b4a595600a02d50df99b585fe2ec18a7
taekoanyoo <tk1star2@nate.com>
2020-01-18 20:13:00 +0900

    revised

--- pytorch/new_docker_pytorch.sh
+++ pytorch/new_docker_pytorch.sh
@@ -1,4 +1,19 @@
-docker run -it --name pyTorch armv7/armhf-ubuntu bash
+sudo xhost +local:root
+
+#docker run -it --name pyTorch armv7/armhf-ubuntu bash
+nvidia-docker run -d -p 6006:6006 -e DISPLAY=$DISPLAY \
+	-p 8888:8888\
+	--env="QT_X11_NO_MITSHM=1"\
+	--net=host \
+	--workdir="/root" \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	-v ~/.Xauthority:/root/.Xauthority:rw \
+	-v /home/tk1star2/Desktop/tk/tk_framework/pytorch/dataset:/root/dataset:rw \
+	--name pyTorch -it nvidia/cuda:9.0-cudnn7-devel-ubuntu16.04 bash
+docker cp ./X11 pyTorch:/root/
+
+
+
 
 #sudo cp /home/tk1star2/.Xauthority /root/
 #sudo xhost +local:root
--- tensorflow2/new_docker_tf.sh
+++ tensorflow2/new_docker_tf.sh
@@ -1,4 +1,4 @@
-sudo cp /home/tk1star2/.Xauthority /root/
+sudo cp ~/.Xauthority /root/
 #docker run -it --name tsflow tk1star2/rpi-docker-tensorflow bash
 sudo xhost +local:root
 
@@ -8,7 +8,7 @@ nvidia-docker run -d -p 6006:6006 -e DISPLAY=$DISPLAY \
 	--net=host \
 	--workdir="/root" \
 	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-	-v /root/.Xauthority:/root/.Xauthority:rw \
+	-v ~/.Xauthority:/root/.Xauthority:rw \
 	-v /home/tk1star2/Desktop/tk/tk_framework/tensorflow2/Desktop:/root/dataset:rw \
 	--name tsflow2 -it nvidia/cuda:9.0-cudnn7-devel-ubuntu16.04 bash
 docker cp ./X11 tsflow2:/root/
